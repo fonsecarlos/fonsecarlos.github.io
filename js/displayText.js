cbdb179c8e141c16bfdf5a686873106748e6a986
Carlos <fonseca.carlosh@gmail.com>
2014-11-18 20:46:40 -0200

    Textos randomicos

---
+++ js/displayText.js
@@ -0,0 +1,62 @@
+(function() {
+      var quotes = [
+        {
+          text: "Indeed, your failure to understand that there are things much worse than death has always been your greatest weakness",
+          //img:  "http://i.stack.imgur.com/FqBE6.jpg?s=32&g=1"
+        },
+        {
+          text: "Beneath this mask there is more than flesh. Beneath this mask there is an idea, Mr. Creedy, and ideas are bulletproof"
+        },
+        {
+          text: "The things you own end up owning you."
+        },
+        {
+          text: "And some things that should not have been forgotten were lost. History became legend; legend became myth."
+        },
+        {
+          text: "I could die right now, Clem. I'm just... happy. I've never felt that before. I'm just exactly where I want to be."
+        },
+        {
+          text: "Eye for eye, tooth for tooth, hand for hand, foot for foot."
+        },
+        {
+          text: "Do not swear by the moon, for she changes constantly. then your love would also change."
+        },
+        {
+          text: "Open your eyes. Maybe you'll see!"
+        },
+        {
+          text: "This is a story of boy meets girl, but you should know upfront, this is not a love story. "
+        },
+        {
+          text: "You're not a bad person. You're a terrific person. You're my favorite person, but every once in a while, you can be a real cunt."
+        }
+      ];
+      var quote = quotes[Math.floor(Math.random() * quotes.length)];
+      document.getElementById("quote").innerHTML =
+        '<p>' + quote.text + '</p>'; //+
+        //'<img src="' + quote.img + '">';
+    })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"Any of you fucking pricks move, and I'll execute every motherfucking last one of ya!"
